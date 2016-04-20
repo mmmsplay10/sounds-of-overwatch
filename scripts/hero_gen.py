2ea9ee817c20e55ba3a59bcecee59ce60b838656
rpboyer15 <rpboyer15@gmail.com>
2016-04-19 23:06:55 -0500

    added scripts

--- scripts/hero_gen.py
+++ scripts/hero_gen.py
@@ -187,6 +187,16 @@ for i in range(ph_count):
 closer ="""
     <script src='../js/jquery-1.12.2.min.js'></script>
     <script src='../js/heroScript.js'></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-67473619-2', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 """
 text += closer
 
